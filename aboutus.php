63fbfdf5685ec846978b9a04dc5fd3fa79617928
vynojusaisiri <88606296+vynojusaisiri@users.noreply.github.com>
2022-12-20 14:13:56 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");
+error_reporting(0);
+session_start();
+
+if(empty($_SESSION['user_id']))  //if usser is not login redirected baack to login page
+{
+	header('location:login.php');
+}
+else
+{
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>Starter Template for Bootstrap</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+<style type="text/css" rel="stylesheet">
+
+
+.indent-small {
+  margin-left: 5px;
+}
+.form-group.internal {
+  margin-bottom: 0;
+}
+.dialog-panel {
+  margin: 10px;
+}
+.datepicker-dropdown {
+  z-index: 200 !important;
+}
+.panel-body {
+  background: #e5e5e5;
+  /* Old browsers */
+  background: -moz-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* FF3.6+ */
+  background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #e5e5e5), color-stop(100%, #ffffff));
+  /* Chrome,Safari4+ */
+  background: -webkit-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* Chrome10+,Safari5.1+ */
+  background: -o-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* Opera 12+ */
+  background: -ms-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* IE10+ */
+  background: radial-gradient(ellipse at center, #e5e5e5 0%, #ffffff 100%);
+  /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e5e5e5', endColorstr='#ffffff', GradientType=1);
+  /* IE6-9 fallback on horizontal gradient */
+  font: 600 15px "Open Sans", Arial, sans-serif;
+}
+
+	</style>
+	</head>
+
+<body>
+    
+<header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="aboutus.php">Aboutus <span class="sr-only"></span></a> </li>
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
+							}
+						else
+							{
+									
+									
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Your Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+
+        <div class="page-wrapper">
+            <!-- top Links -->
+            <div class="top-links">
+                <div class="container">
+                    <ul class="row links">
+                       
+                        <li class="col-xs-12 col-sm-4 link-item active"><span>1</span><a href="restaurants.php">Choose Restaurant</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item"><span>2</span><a href="#">Pick Your favorite food</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item"><span>3</span><a href="#">Order and Pay online</a></li>
+                    </ul>
+                </div>
+            </div>
+           
+            <!-- end:Top links -->
+            <!-- start: Inner page hero -->
+            <div class="inner-page-hero bg-image" data-image-src="images/img/res.jpeg">
+                <div class="container"> </div>
+                <!-- end:Container -->
+            </div>
+            <div class="result-show">
+                <div class="container">
+                    <div class="row">
+                       
+                       
+                    </div>
+                </div>
+            </div>
+            <!-- //results show -->
+            <section class="restaurants-page">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-3">
+                          
+                          
+                            <div class="widget clearfix">
+                                <!-- /widget heading -->
+                                <div class="widget-heading">
+                                    <h3 class="widget-title text-dark">
+                                 Popular tags
+                              </h3>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="clearfix"></div>
+                                </div>
+                                <div class="widget-body">
+                                    <ul class="tags">
+                                        <li> <a href="#" class="tag">
+                                    Chicken
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Roasted chicken
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Grilled chicken
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Mutton
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Biryani
+                                    </a> </li>
+                                        <li> <a href="#" class="tag">
+                                    Fire chicken
+                                    </a> </li>
+                                    </ul>
+                                </div>
+                            </div>
+                    </div>
+                
+            <center>
+         <h2 >Restaurant &nbsp; Overview</h2>
+         <hr class="heading_space">
+         <p>Launched in Mumbai, Food Picky has grown from a home project to one of the largest food aggregators in the world. We are present in 24 countries and 10000+ cities globally, enabling our vision of better food for more people. We not only connect people to food in every context but work closely with restaurants to enable a sustainable ecosystem.</p>
+        <br/>
+        <h2>Register to BE AN AFFILIATE MARKETER</h1>
+        <button onclick="location.href='marketerregister.php'" type="button" class="btn theme-btn btn-lg">Click on Register</button>
+        <br/>
+        <br/>
+        <h2>Already a MARKETER ?</h1>
+        <button onclick="location.href='signin.php'" type="button" class="btn theme-btn btn-lg">Login</button>
+                        </center>
+                        </div>
+            </section>
+            <section class="app-section">
+                <div class="app-wrap">
+                    <div class="container">
+                        <div class="row text-img-block text-xs-left">
+                            <div class="container">
+                                <div class="col-xs-12 col-sm-6 hidden-xs-down right-image text-center">
+                                    <figure> <img src="images/app.png" alt="Right Image"> </figure>
+                                </div>
+                                <div class="col-xs-12 col-sm-6 left-text">
+                                    <h3>The Best Food Delivery App</h3>
+                                    <p>Now you can make food happen pretty much wherever you are thanks to the free easy-to-use Food Delivery &amp; Takeout App.</p>
+                                    <div class="social-btns">
+                                        <a href="#" class="app-btn apple-button clearfix">
+                                            <div class="pull-left"><i class="fa fa-apple"></i> </div>
+                                            <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">App Store</span> </div>
+                                        </a>
+                                        <a href="#" class="app-btn android-button clearfix">
+                                            <div class="pull-left"><i class="fa fa-android"></i> </div>
+                                            <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">Play store</span> </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- start: FOOTER -->
+            <footer class="footer">
+                <div class="container">
+                    <!-- top footer statrs -->
+                    <div class="row top-footer">
+                        <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                            <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
+                        <div class="col-xs-12 col-sm-2 about color-gray">
+                            <h5>About Us</h5>
+                            <ul>
+                                <li><a href="#">About us</a> </li>
+                                <li><a href="#">History</a> </li>
+                                <li><a href="#">Our Team</a> </li>
+                                <li><a href="#">We are hiring</a> </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                            <h5>How it Works</h5>
+                            <ul>
+                                <li><a href="#">Enter your location</a> </li>
+                                <li><a href="#">Choose restaurant</a> </li>
+                                <li><a href="#">Choose meal</a> </li>
+                                <li><a href="#">Pay via credit card</a> </li>
+                                <li><a href="#">Wait for delivery</a> </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-2 pages color-gray">
+                            <h5>Pages</h5>
+                            <ul>
+                                <li><a href="#">Search results page</a> </li>
+                                <li><a href="#">User Sing Up Page</a> </li>
+                                <li><a href="#">Pricing page</a> </li>
+                                <li><a href="#">Make order</a> </li>
+                                <li><a href="#">Add to cart</a> </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-3 popular-locations color-gray">
+                            <h5>Popular locations</h5>
+                            <ul>
+                                <li><a href="#">Sarajevo</a> </li>
+                                <li><a href="#">Split</a> </li>
+                                <li><a href="#">Tuzla</a> </li>
+                                <li><a href="#">Sibenik</a> </li>
+                                <li><a href="#">Zagreb</a> </li>
+                                <li><a href="#">Brcko</a> </li>
+                                <li><a href="#">Beograd</a> </li>
+                                <li><a href="#">New York</a> </li>
+                                <li><a href="#">Gradacac</a> </li>
+                                <li><a href="#">Los Angeles</a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- top footer ends -->
+                    <!-- bottom footer statrs -->
+                    <div class="row bottom-footer">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                                    <h5>Payment Options</h5>
+                                    <ul>
+                                        <li>
+                                            <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                        </li>
+                                        <li>
+                                            <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                        </li>
+                                        <li>
+                                            <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                        </li>
+                                        <li>
+                                            <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                        </li>
+                                        <li>
+                                            <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5>Address</h5>
+                                    <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
+                                    <h5>Phone: <a href="tel:+080000012222">080 000012 222</a></h5> </div>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5>Addition informations</h5>
+                                    <p>Join the thousands of other restaurants who benefit from having their menus on TakeOff</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- bottom footer ends -->
+                </div>
+            </footer>
+            <!-- end:Footer -->
+        </div>
+  
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
+<?php
+}
+?>
